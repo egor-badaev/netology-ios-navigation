1ba682c78506a905ac6c9810743ba7c41d50d11b
Artem Novichkov <novichkoff93@gmail.com>
2020-09-12 17:03:24 +0600

    Initial Commit

---
+++ Navigation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Artem Novichkov on 12.09.2020.
+//  Copyright © 2020 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
