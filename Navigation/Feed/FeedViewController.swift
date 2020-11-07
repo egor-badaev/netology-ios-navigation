6065a13c2703ffd31d33709daf7f94fbb0de152e
Egor Badaev <egor.badaev@gmail.com>
2020-11-08 00:06:53 +0300

    HW2.2 Part 2 (advanced) Complete

---
+++ Navigation/Feed/FeedViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Artem Novichkov on 12.09.2020.
+//  Copyright © 2020 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+final class FeedViewController: UIViewController {
+
+    // MARK: - properties
+    
+    private let posts: [Post] = {
+        var posts: [Post] = []
+
+        /**
+         По условиям задачи добавить нужно всего две кнопки, но текущая модель
+         позволяет добавлять сколько угодно кнопок (пока не закончится экран)
+
+         Для этого нужно добавлять эелементы в массив `posts` - для каждого
+         из них создастся кнопка с переходом на соответствующий пост
+         */
+        posts.append(Post(title: "Пост 1"))
+        posts.append(Post(title: "Пост 2"))
+
+        return posts
+    }()
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let stackView = UIStackView()
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+
+        stackView.axis = .vertical
+        stackView.spacing = 10.0
+
+        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+
+        for (index, _) in posts.enumerated() {
+            let postButton = buttonForPost(index: index)
+            stackView.addArrangedSubview(postButton)
+        }
+    }
+
+    // MARK: - Private methods
+
+    private func buttonForPost(index: Int) -> PostButton {
+        let button = PostButton()
+
+        button.setTitle("Show post \(index + 1)", for: .normal)
+        button.sizeToFit()
+        button.addTarget(self, action: #selector(buttonTapped(sender:)), for: .touchUpInside)
+
+        button.post = posts[index]
+
+        return button
+    }
+
+    @objc private func buttonTapped(sender: Any) {
+
+        guard let navigationController = self.navigationController,
+              let storyboard = self.storyboard else {
+            return
+        }
+
+        guard let postViewController = storyboard.instantiateViewController(withIdentifier: String(describing: PostViewController.self)) as? PostViewController else {
+            return
+        }
+
+        guard let postButton = sender as? PostButton,
+              let post = postButton.post else {
+            return
+        }
+
+        postViewController.post = post
+
+        navigationController.pushViewController(postViewController, animated: true)
+    }
+}
---
+++ Navigation/Feed/PostButton.swift
@@ -0,0 +1,15 @@
+//
+//  PostButton.swift
+//  Navigation
+//
+//  Created by Egor Badaev on 07.11.2020.
+//  Copyright © 2020 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class PostButton: UIButton {
+
+    var post: Post? = nil
+
+}
