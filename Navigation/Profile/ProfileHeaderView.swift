60925b7e40f3c0dd272b2a91d4167ffc76148f4b
Egor Badaev <egor.badaev@gmail.com>
2020-11-23 22:13:00 +0300

    HW2.3 corrections: migrate ProfileHeaderView to non-xib auto-layout

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -12,42 +12,88 @@ class ProfileHeaderView: UIView {
 
     // MARK: - Constants
 
-    private let radius: CGFloat = 4.0
-    private let defaultStatusText = "Waiting for something..."
+    private enum Config {
+        static let largeMargin: CGFloat = 27.0
+        static let radius: CGFloat = 4.0
+        static let avatarSize: CGFloat = 110.0
+        static let textFieldHeight: CGFloat = 40.0
+        static let statusButtonHeight: CGFloat = 50.0
+        static let defaultStatusText = "Waiting for something..."
+    }
 
-    // MARK: - UI
+    // MARK: - Setup UI
 
-    @IBOutlet private var avatarImageView: UIImageView! {
-        didSet {
-            avatarImageView.layer.cornerRadius = avatarImageView.bounds.width / 2
-            avatarImageView.layer.borderWidth = 3
-            avatarImageView.layer.borderColor = UIColor.white.cgColor
-        }
-    }
-    @IBOutlet private var titleLabel: UILabel!
-    @IBOutlet private var statusLabel: UILabel! {
-        didSet {
-            // В IB нет цвета gray! Есть systemGray, есть lightGray, darkGray, но просто gray нет
-            statusLabel.textColor = .gray
-            statusLabel.text = defaultStatusText
-        }
-    }
-    @IBOutlet private var statusTextField: StatusTextField! {
-        didSet {
-            statusTextField.layer.masksToBounds = true
-            statusTextField.layer.addSublayer(statusTextFieldBackgroundLayer)
-        }
-    }
-    @IBOutlet private var statusButton: UIButton! {
-        didSet {
-            statusButton.layer.masksToBounds = false
-            statusButton.layer.cornerRadius = radius
-            statusButton.layer.shadowOffset = CGSize(width: radius, height: radius)
-            statusButton.layer.shadowRadius = radius
-            statusButton.layer.shadowColor = UIColor.black.cgColor
-            statusButton.layer.shadowOpacity = 0.7
-        }
-    }
+    private lazy var avatarImageView: UIImageView = {
+        let avatarImageView = UIImageView()
+
+        avatarImageView.toAutoLayout()
+        avatarImageView.image = #imageLiteral(resourceName: "profilePhoto")
+        avatarImageView.layer.cornerRadius = Config.avatarSize / 2
+        avatarImageView.layer.borderWidth = 3
+        avatarImageView.layer.borderColor = UIColor.white.cgColor
+        avatarImageView.clipsToBounds = true
+
+        return avatarImageView
+    }()
+
+    private let titleLabel: UILabel = {
+        let titleLabel = UILabel()
+
+        titleLabel.toAutoLayout()
+        titleLabel.text = "John Appleseed"
+        titleLabel.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        titleLabel.textColor = .black
+        titleLabel.sizeToFit()
+
+        return titleLabel
+    }()
+
+    private lazy var statusLabel: UILabel = {
+        let statusLabel = UILabel()
+
+        statusLabel.toAutoLayout()
+        statusLabel.text = Config.defaultStatusText
+        statusLabel.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        statusLabel.textColor = .gray
+        statusLabel.sizeToFit()
+
+        return statusLabel
+
+    }()
+
+    private lazy var statusButton: UIButton = {
+        let statusButton = UIButton(type: .system)
+
+        statusButton.toAutoLayout()
+        statusButton.backgroundColor = .systemBlue
+        statusButton.setTitleColor(.white, for: .normal)
+        statusButton.setTitle("Set status", for: .normal)
+        statusButton.clipsToBounds = true
+        statusButton.layer.masksToBounds = false
+        statusButton.layer.cornerRadius = Config.radius
+        statusButton.layer.shadowOffset = CGSize(width: Config.radius, height: Config.radius)
+        statusButton.layer.shadowRadius = Config.radius
+        statusButton.layer.shadowColor = UIColor.black.cgColor
+        statusButton.layer.shadowOpacity = 0.7
+        statusButton.addTarget(self, action: #selector(statusButtonTapped(_:)), for: .touchUpInside)
+
+        return statusButton
+    }()
+
+    private lazy var statusTextField: StatusTextField = {
+        let statusTextField = StatusTextField()
+
+        statusTextField.toAutoLayout()
+        statusTextField.textColor = .black
+        statusTextField.font = UIFont.systemFont(ofSize: 15.0)
+        statusTextField.placeholder = "Set your status..."
+        statusTextField.layer.masksToBounds = true
+        statusTextField.layer.addSublayer(statusTextFieldBackgroundLayer)
+
+        statusTextField.addTarget(self, action: #selector(statusTextFieldChanged(_:)), for: .editingChanged)
+
+        return statusTextField
+    }()
 
     private let statusTextFieldBackgroundLayer: CALayer = {
 
@@ -62,12 +108,18 @@ class ProfileHeaderView: UIView {
         return statusTextFieldBackgroundLayer
     }()
     
-    private lazy var statusText: String = defaultStatusText
+    private lazy var statusText: String = Config.defaultStatusText
 
     // MARK: - Life cycle
 
     required init?(coder: NSCoder) {
-        super.init(coder: coder)
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setupUI()
     }
 
     override func layoutSubviews() {
@@ -81,16 +133,56 @@ class ProfileHeaderView: UIView {
         )
     }
 
-    // MARK: - Actions
+    // MARK: - Private methods
 
-    @IBAction func statusButtonTapped(_ sender: Any) {
+    private func setupUI() {
+
+        backgroundColor = .lightGray
+
+        addSubview(avatarImageView)
+        addSubview(titleLabel)
+        addSubview(statusLabel)
+        addSubview(statusTextField)
+        addSubview(statusButton)
+
+        let constraints = [
+            avatarImageView.topAnchor.constraint(equalTo: topAnchor, constant: AppConstants.margin),
+            avatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: AppConstants.margin),
+            avatarImageView.heightAnchor.constraint(equalToConstant: Config.avatarSize),
+            avatarImageView.widthAnchor.constraint(equalTo: avatarImageView.heightAnchor),
+
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: Config.largeMargin),
+            titleLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: AppConstants.margin),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -AppConstants.margin),
+
+            statusButton.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 43.0),
+            statusButton.leadingAnchor.constraint(equalTo: avatarImageView.leadingAnchor),
+            statusButton.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
+            statusButton.heightAnchor.constraint(equalToConstant: Config.statusButtonHeight),
+            statusButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -AppConstants.margin),
+
+            statusTextField.bottomAnchor.constraint(equalTo: statusButton.topAnchor, constant: -AppConstants.margin),
+            statusTextField.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            statusTextField.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
+            statusTextField.heightAnchor.constraint(equalToConstant: Config.textFieldHeight),
+
+            statusLabel.bottomAnchor.constraint(equalTo: statusTextField.topAnchor, constant: -6.0),
+            statusLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            statusLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor)
+        ]
+
+        NSLayoutConstraint.activate(constraints)
+
+    }
+
+    @objc private func statusButtonTapped(_ sender: Any) {
         statusLabel.text = statusText
 
         // Очистить текстовое поле после установки статуса
         statusTextField.text = ""
     }
 
-    @IBAction func statusTextFieldChanged(_ sender: Any) {
+    @objc private func statusTextFieldChanged(_ sender: Any) {
         guard let textField = sender as? StatusTextField,
               let statusText = textField.text else {
             print("No status has been entered!")
